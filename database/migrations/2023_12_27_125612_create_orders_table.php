92f96269a91be14280d312338bca0adf60964d56
Guaiche Achraf <achrafguaiche@gmail.com>
2023-12-27 14:41:38 +0100

    Scrum 1: Create Models, Migrations | Scrum 7: Create Diagram Class, Sequences Digram

---
+++ database/migrations/2023_12_27_125612_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->double('total_price');
+            $table->unsignedBigInteger('coupon_id');
+            $table->foreign('coupon_id')->references('id')->on('coupons');
+            $table->unsignedBigInteger('shipping_address_id')->nullable();
+            $table->foreign('shipping_address_id')->references('id')->on('shipping_addresses');
+            $table->string('order_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
